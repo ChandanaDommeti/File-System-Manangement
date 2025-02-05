56905744b7b6c77a8e069417d1b7881b68372474
ChandanaDommeti <145481169+ChandanaDommeti@users.noreply.github.com>
2025-02-05 19:21:01 +0530

    Add files via upload

---
+++ Filesystem.java
@@ -0,0 +1,16 @@
+package login;
+
+public class File{
+	String FileName;
+	File add;
+	File remove;
+	public void File(String s) {
+		this.FileName=s;
+		this.add=add;
+		this.remove=remove;
+	}
+}
+
+public class Filesystem {
+
+}
